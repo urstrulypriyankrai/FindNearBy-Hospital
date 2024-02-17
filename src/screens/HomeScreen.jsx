8e1b39318e4fb147c97307bfbe938dd8df946b26
priyank rai <type2email@gmail.com>
2024-02-17 19:18:33 +0530

    getting current location

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,51 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-
+import {StyleSheet, Text, View, PermissionsAndroid, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Geolocation from 'react-native-geolocation-service';
+/**********End of Imports *********/
 const HomeScreen = () => {
+  useEffect(() => {
+    requestGeolocationPermission();
+  }, []);
+  const [currentPosition, setCurrentPosition] = useState({
+    latitude: null,
+    longitude: null,
+  });
+  const requestGeolocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'NearBy Hospital App Geolocation Permission',
+          message:
+            'App Needs Your Current Location ' +
+            'In order to find nearby hospials.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the Location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      error => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
   return (
     <View>
       <Text>HomeScreen</Text>
